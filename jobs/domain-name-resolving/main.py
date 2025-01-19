449f10c2bdf5cd58ce2438706309d84986dd1131
lm-sec <sim.lacasse1239@gmail.com>
2025-01-19 15:10:46 -0500

    batching the domain name resolution job

--- jobs/domain-name-resolving/main.py
+++ jobs/domain-name-resolving/main.py
@@ -1,17 +1,29 @@
 import os
 import socket
+import json
 
-from stalker_job_sdk import DomainFinding, JobStatus, log_finding, log_status
+from stalker_job_sdk import DomainFinding, JobStatus, log_finding, log_status, log_error
 
-hostname = os.environ.get("domainName")
-data = socket.gethostbyname_ex(hostname)
-ipx = data[2]
 
-for ip in ipx:
-    log_finding(
-        DomainFinding(
-            "HostnameIpFinding", hostname, ip, "New ip", [], "HostnameIpFinding"
-        )
-    )
+def main():
+    hostnames = json.loads(os.environ.get("domainNames"))
 
-log_status(JobStatus.SUCCESS)
+    for hostname in hostnames:
+        data = socket.gethostbyname_ex(hostname)
+        ipx = data[2]
+
+        for ip in ipx:
+            log_finding(
+                DomainFinding(
+                    "HostnameIpFinding", hostname, ip, "New ip", [], "HostnameIpFinding"
+                )
+            )
+
+try:
+    main()
+    log_status(JobStatus.SUCCESS)
+except Exception as err:
+    log_error("An unexpected error occured")
+    log_error(err)
+    log_status(JobStatus.FAILED)
+    exit()
