d6402ed5a469389910f1e4e422f5e2925e6b5656
lm-sec <sim.lacasse1239@gmail.com>
2026-02-01 23:12:38 -0500

    applying hotfix

--- jobs/domain-name-resolving/main.py
+++ jobs/domain-name-resolving/main.py
@@ -2,22 +2,25 @@ import os
 import socket
 import json
 
-from stalker_job_sdk import DomainFinding, JobStatus, log_finding, log_status, log_error
+from stalker_job_sdk import DomainFinding, JobStatus, log_finding, log_status, log_error, log_warning
 
 
 def main():
     hostnames = json.loads(os.environ.get("domainNames"))
 
     for hostname in hostnames:
-        data = socket.gethostbyname_ex(hostname)
-        ipx = data[2]
+        try:
+            data = socket.gethostbyname_ex(hostname)
+            ipx = data[2]
 
-        for ip in ipx:
-            log_finding(
-                DomainFinding(
-                    "HostnameIpFinding", hostname, ip, "New ip", [], "HostnameIpFinding"
+            for ip in ipx:
+                log_finding(
+                    DomainFinding(
+                        "HostnameIpFinding", hostname, ip, "New ip", [], "HostnameIpFinding"
+                    )
                 )
-            )
+        except:
+            log_warning(f"Domain name {hostname} did not resolve.")
 
 try:
     main()
